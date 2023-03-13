5a39cca224a223065deaaa6f6b3bdbdc7e8a80cd
gio <giovany.torres.799@gmail.com>
2023-03-13 17:22:45 -0500

    updatedreadme.md

---
+++ awaitToCallback.js
@@ -0,0 +1,31 @@
+import {
+    doTask1,
+    doTask2,
+    doTask3
+} from './tasks';
+
+export function runCode() {
+    const strings = [];
+
+    
+    return doTask1()
+        .then(res => {
+            strings.push(res)
+            return doTask2();
+        })
+        .then(res => {
+            strings.push(res)
+            return doTask3();
+        })
+        .then(res => {
+            strings.push(res)
+            return strings
+        })
+        .catch(error => {
+            console.log(error)
+        });
+}
+
+
+runCode()
+    .then(response => console.log(response));
