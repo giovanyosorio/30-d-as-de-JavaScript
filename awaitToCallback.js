cfcbc3ce64541baa2aaaa982a8fcb1918109417c
giovany osorio <giovany.torres.799@gmail.com>
2023-03-14 22:51:18 -0500

    ejercico nuevo fizzbuzz

--- awaitToCallback.js
+++ awaitToCallback.js
@@ -4,28 +4,28 @@ import {
     doTask3
 } from './tasks';
 
-export function runCode() {
+export async function runCode() {
     const strings = [];
-
-    
     return doTask1()
-        .then(res => {
-            strings.push(res)
-            return doTask2();
-        })
-        .then(res => {
-            strings.push(res)
-            return doTask3();
-        })
-        .then(res => {
-            strings.push(res)
-            return strings
-        })
-        .catch(error => {
-            console.log(error)
-        });
+    .then(rta1=>{strings.push(rta1);
+    return doTask2()})
+    .then(rta2=>{strings.push(rta2);
+    return doTask3()})
+    .then(rta3=>{strings.push(rta3);
+        return strings})
+    
+
+        
+    await doTask1((rta1)=>{strings.push(rta1)})
+    await doTask2((rta2)=>{strings.push(rta2)})
+    await doTask3((rta2)=>{strings.push(rta3)})
+        return strings
+
+    let result1 = await doTask1();
+    strings.push(result1);
+    let result2 = await doTask2();
+    strings.push(result2);
+    let result3 = await doTask3();
+    strings.push(result3);
+    return strings
 }
-
-
-runCode()
-    .then(response => console.log(response));
