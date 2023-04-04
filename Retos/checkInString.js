2eb885112794c798122db3156091bed6037bf29c
giovanyosorio <giovany.torres.799@gmail.com>
2023-04-04 17:00:37 -0500

    Busca dentro de un string y descubre si ¿Tiene la clave?

---
+++ Retos/checkInString.js
@@ -0,0 +1,8 @@
+function checkInString(text, term) {
+    // Tu código aquí 👈
+
+    if (term.includes(text)) {
+            console.log("hola mundo");
+    }
+  }
+checkInString("ana lava la tina", "ana")  
