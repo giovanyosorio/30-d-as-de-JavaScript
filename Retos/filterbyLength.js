f0041e37bf67079bf1a5e5d03d25262568567129
giovanyosorio <giovany.torres.799@gmail.com>
2023-04-05 16:35:25 -0500

    filterbyLength

---
+++ Retos/filterbyLength.js
@@ -0,0 +1,8 @@
+function filterByLength(array) {
+    // Tu código aquí 👈
+    //return array.filter((item)=>item.length>=4)
+    console.log(array.filter((item)=>item.length>=4));
+  }
+
+  filterByLength(['amor', 'sol', 'piedra', 'día'])
+  filterByLength(['rosa', 'gol', 'pez', 'día', 'gafas'])
