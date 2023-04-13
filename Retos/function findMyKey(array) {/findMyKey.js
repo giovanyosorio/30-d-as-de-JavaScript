f0318f46cd83801c5f1c44068209713db2118a88
giovany osorio <giovany.torres.799@gmail.com>
2023-04-13 12:19:54 -0500

    Retorna la posición de una llave dentro de una lista

--- Retos/function findMyKey(array) {/findMyKey.js
+++
@@ -1,7 +0,0 @@
-function findMyKey(array) {
-  // Tu código aquí 👈
-  const find = array.findIndex((element) => element === "myKey")
-
-  if (find == -1 || array.length == 0){ return false }
-  else { return find }
-}
